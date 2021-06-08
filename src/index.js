394d9c56c9d4a046811ae05f57a2c12eabbb92b1
DanielBradleyBennett <danielbradleybennett@gmail.com>
2021-06-07 20:51:05 -0500

    board component

--- src/index.js
+++ src/index.js
@@ -15,16 +15,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-
-
   handleClick(i) {
     const squares = this.state.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -37,11 +27,12 @@ class Board extends React.Component {
     });
   }
 
+
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
